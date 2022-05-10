4969e6159dd043a79a2688a5ca2aca65b84325ee
Hasosoon <hasanemirr15@gmail.com>
2022-05-10 15:07:01 +0100

    New array pojo

--- src/test/java/apitests/hrStudying/hr/hrAttraction.java
+++ src/test/java/apitests/hrStudying/hr/hrAttraction.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import utilities.ConfigurationReader;
 
 import java.util.Arrays;
-import java.util.Map;
+import java.util.List;
 
 import static io.restassured.RestAssured.baseURI;
 
@@ -30,6 +30,19 @@ public class hrAttraction {
         String eu = allRego.getItems().get(0).getregion_name();
         System.out.println("eu = " + eu);
 
+        List<Item> link = allRego.getItems();
+        for (int i = 0; i < link.size(); i++) {
+
+            String href = allRego.getItems().get(i).getLinks().get(0).getHref();
+            String[] str = href.split("/");
+            System.out.println("Arrays.toString(str) = " + Arrays.toString(str));
+            int id = Integer.parseInt(str[str.length - 1]);
+            int regionId = allRego.getItems().get(i).getregion_id();
+
+            Assert.assertEquals("Not True",id,regionId);
+
+        }
+        /*
         String href = allRego.getItems().get(1).getLinks().get(0).getHref();
         String[] str = href.split("/");
         System.out.println("Arrays.toString(str) = " + Arrays.toString(str));
@@ -38,7 +51,23 @@ public class hrAttraction {
 
         Assert.assertEquals("Not True",id,regionId);
 
+         */
 
         Assert.assertEquals(response.statusCode(),200);
     }
+    @Test
+    public void test2(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get("/regions");
+        AllRego allRego = response.body().as(AllRego.class);
+        String Actrel = allRego.getLinks().get(2).getRel();
+        System.out.println("Actrel = " + Actrel);
+        String ExRel = "describedby";
+        System.out.println("ExRel = " + ExRel);
+
+        Assert.assertTrue(ExRel.equals(Actrel));
+
+    }
+
 }
